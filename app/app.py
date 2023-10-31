1edac3478c7e36d2085af9bd75a534e507511af5
maleyy <wapzif@gmail.com>
2023-10-31 22:40:24 +0100

    update

--- app/app.py
+++ app/app.py
@@ -2,81 +2,64 @@ import json
 import datetime
 import urllib.request
 import mariadb
+import os
 
 from icecream import ic
 from flask import Flask, send_file, request, jsonify
 
 app = Flask(__name__)
 
-# Modify the database connection configuration for MariaDB
+# Database configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'caca'
 app.config['MYSQL_PASSWORD'] = 'caca'
 app.config['MYSQL_DB'] = 'mydb'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # Use a dictionary cursor for easier data access
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-def fetch_data(ip):
-    url = f"http://ipinfo.io/{ip}?token=95c708f8827e83"
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    data = json.loads(data)
-    return data
-
-
-def duplicate_check(ip):
-    conn = mariadb.connect(
+def establish_db_connection():
+    return mariadb.connect(
         host=app.config['MYSQL_HOST'],
         user=app.config['MYSQL_USER'],
         password=app.config['MYSQL_PASSWORD'],
         database=app.config['MYSQL_DB']
     )
-    cursor = conn.cursor()
 
+def fetch_data(ip):
+    url = f"http://ipinfo.io/{ip}?token=95c708f8827e83"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+    return data
+
+def duplicate_check(ip):
+    conn = establish_db_connection()
+    cursor = conn.cursor()
     query = "SELECT * FROM Informations WHERE IP=%s"
     cursor.execute(query, (ip,))
     result = cursor.fetchone()
     conn.close()
     return result
 
-
 def insert_data(data, time_stamp, user_agent):
-
-    conn = mariadb.connect (
-        host=app.config['MYSQL_HOST'],
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        database=app.config['MYSQL_DB']
-    )
-
+    conn = establish_db_connection()
     cursor = conn.cursor()
 
     if duplicate_check(data['ip']):
-        # If the IP already exists in the database, modify the timestamp
-
         query = "UPDATE Informations SET TimeStamp=%s WHERE IP=%s"
         values = (time_stamp, data['ip'])
-        cursor.execute(query, values)
-        conn.commit()
-        conn.close()
-        return
-
-    # If the IP does not exist in the database, insert the data
     else:
-    query = "INSERT INTO Informations(IP,City,Region,ZIP,Localisation,Organization,TimeZone,TimeStamp,User_Agent,Country) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-    ic(data, time_stamp, user_agent)
-    values = (
-        data['ip'],
-        data['city'],
-        data['region'].encode("ascii",'ignore'),
-        data['postal'],
-        data['loc'],
-        data['org'],
-        data['timezone'],
-        time_stamp,
-        user_agent,
-        data['country']
-    )
+        query = "INSERT INTO Informations(IP,City,Region,ZIP,Localisation,Organization,TimeZone,TimeStamp,User_Agent,Country) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (
+            data['ip'],
+            data['city'],
+            data['region'].encode("ascii", 'ignore'),
+            data['postal'],
+            data['loc'],
+            data['org'],
+            data['timezone'],
+            time_stamp,
+            user_agent,
+            data['country']
+        )
 
     cursor.execute(query, values)
     conn.commit()
